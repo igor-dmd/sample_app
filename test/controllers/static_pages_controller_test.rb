47d9e11c8b9075f03f1e31c804c28d8f8fb41529
Igor de Medeiros <igormedeiros0@gmail.com>
2015-11-05 09:59:33 -0300

    Resolves the conflict at the test controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> static-pages
   test "should get home" do
     get :home
     assert_response :success
